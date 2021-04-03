2e60bb45fa296336f07fdb52dbf985b9f0fc2f66
SONNUR TAYFUROĞLU <2014141005@cumhuriyet.edu.tr>
2021-04-03 13:47:27 +0300

    updated

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -53,6 +54,7 @@ namespace WebAPI.Controllers
 		[HttpGet("getall")]
 		public IActionResult GetAll()
 		{
+			Thread.Sleep(3000);
 			var result = _carService.GetAll();
 			if (result.Success)
 			{
